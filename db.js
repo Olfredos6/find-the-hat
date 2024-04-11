c5d29ed71b3970a28022da014d2b17674f2e371a
Nehemie Balukidi <alfred.nehemie@gmail.com>
2024-04-11 11:42:40 +0200

    fix: auto create profiles table
    fix: use common error logger function
    
    chore: some cleanup

--- db.js
+++ db.js
@@ -8,30 +8,66 @@ const DB_CONFIG = {
   database: process.env.DB_NAME
 }
 
+const QUERY_CREATE_TABLE_PROFILES = `CREATE TABLE IF NOT EXISTS profiles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
+  name VARCHAR(100) NOT NULL
+)`;
 
-const QUERY_CREATE_TABLE_SCORE = `CREATE TABLE IF NOT EXISTS scores (
+const QUERY_CREATE_TABLE_SCORE = `CREATE TABLE scores (
   id INT AUTO_INCREMENT PRIMARY KEY,
   datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
   field_size INT,
   hole_count INT,
   steps INT,
-  score DECIMAL(10, 2) GENERATED ALWAYS AS (
+  score INT GENERATED ALWAYS AS (
       CASE
           WHEN steps > 0 THEN ROUND((field_size - hole_count) / steps)
           ELSE 0
       END
   ),
-  is_win BOOLEAN
-)`
+  is_win BOOLEAN,
+  profile INT,
+  FOREIGN KEY(profile) REFERENCES profiles(id)
+)`;
 
+function logErrorMessage(err){
+  console.log(err.errno?.toString, err.message)
+}
 
 module.exports = new (function () {
 
   return {
     connection: mysql.createConnection(DB_CONFIG),
-    createScoresTable: function(){
+    createScoresTableIfNotExists: function(){
       return this.connection.promise().query(QUERY_CREATE_TABLE_SCORE)
-      .catch(e => {console.log(e)})
+      .catch(e => { logErrorMessage(e) })
+    },
+    createProfilesTableIfNotExists: function(){
+      return this.connection.promise().query(QUERY_CREATE_TABLE_PROFILES)
+      .catch(e => { logErrorMessage(e) })
+    },
+    DBSetup: function(){
+      this.createProfilesTableIfNotExists()
+      .then(_ => {
+        this.createScoresTableIfNotExists();
+      })
+      .catch(e => { logErrorMessage(e) })
+    },
+    selectProfiles: function(){
+      this.connection.promise().query("SELECT * FROM profiles")
+      .catch(e => { logErrorMessage(e) })
+    },
+    insertScore: function(field_size, hole_count, steps, is_win){
+      return this.connection.promise()
+      .query(
+        `INSERT INTO scores (field_size, hole_count, steps, is_win) VALUES (?, ?, ?, ?)`, 
+        [field_size, hole_count, steps, is_win]
+      )
+      .then((results) => {
+        return results;
+      })
+      .catch(e => { logErrorMessage(e) })
     }
   }
 })();
